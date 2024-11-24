db09b68da980aa88db436a1c317b0851d3a650ff
AntonimusCrack <gumy080327hnetrrne@cbtis217.edu.mx>
2024-11-24 17:11:52 -0600

    tapetes

---
+++ Tapetess/Tapetess/MainForm.Designer.cs
@@ -0,0 +1,345 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yurem
+ * Date: 24/11/2024
+ * Time: 03:46 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Tapetess
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(440, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(334, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "CALCULO DE  MATERIALES PARA TAPETES";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(398, 64);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(448, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "INGRESE EL TAMAÑO EN METROS (LARGO POR ANCHO)";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(431, 101);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(674, 101);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(581, 100);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(39, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "X";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(71, 136);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(191, 23);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "BULTO DE ASERRIN";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(300, 136);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(297, 23);
+			this.label7.TabIndex = 9;
+			this.label7.Text = "FRUTOS SECOS SURTIDOS (KILOGRAMO)";
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(653, 136);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(155, 23);
+			this.label8.TabIndex = 10;
+			this.label8.Text = "LITRO DE PINTURA";
+			// 
+			// label9
+			// 
+			this.label9.Location = new System.Drawing.Point(854, 136);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(184, 23);
+			this.label9.TabIndex = 11;
+			this.label9.Text = "DOCENA DE FLORES";
+			// 
+			// label10
+			// 
+			this.label10.Location = new System.Drawing.Point(516, 228);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(189, 23);
+			this.label10.TabIndex = 12;
+			this.label10.Text = "LITRO DE AGUA (GRIFO)";
+			// 
+			// label14
+			// 
+			this.label14.Location = new System.Drawing.Point(126, 159);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(100, 23);
+			this.label14.TabIndex = 22;
+			this.label14.Text = "370$";
+			// 
+			// label15
+			// 
+			this.label15.Location = new System.Drawing.Point(419, 159);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(100, 23);
+			this.label15.TabIndex = 23;
+			this.label15.Text = "250$";
+			// 
+			// label16
+			// 
+			this.label16.Location = new System.Drawing.Point(674, 159);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(100, 23);
+			this.label16.TabIndex = 24;
+			this.label16.Text = "200$";
+			// 
+			// label17
+			// 
+			this.label17.Location = new System.Drawing.Point(879, 159);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(100, 23);
+			this.label17.TabIndex = 25;
+			this.label17.Text = "450$";
+			// 
+			// label18
+			// 
+			this.label18.Location = new System.Drawing.Point(561, 251);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(100, 23);
+			this.label18.TabIndex = 26;
+			this.label18.Text = "9$";
+			// 
+			// label11
+			// 
+			this.label11.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.label11.Location = new System.Drawing.Point(498, 302);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(207, 24);
+			this.label11.TabIndex = 27;
+			this.label11.Text = "COSTO DE MANO DE OBRA";
+			// 
+			// label12
+			// 
+			this.label12.Location = new System.Drawing.Point(431, 338);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(325, 23);
+			this.label12.TabIndex = 28;
+			this.label12.Text = "COSTO DE MANO DE OBRA (POR PERSONA)";
+			// 
+			// label19
+			// 
+			this.label19.Location = new System.Drawing.Point(561, 361);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(100, 23);
+			this.label19.TabIndex = 29;
+			this.label19.Text = "400$";
+			// 
+			// label13
+			// 
+			this.label13.Location = new System.Drawing.Point(300, 416);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(564, 23);
+			this.label13.TabIndex = 30;
+			this.label13.Text = "SE AÑADE UN COSTO DE 55 PESOS PARA LA COMIDA DE LAS PERSONAS";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(561, 441);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(84, 23);
+			this.button1.TabIndex = 31;
+			this.button1.Text = "TOTAL";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(131, 201);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 22);
+			this.textBox3.TabIndex = 32;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(407, 201);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 22);
+			this.textBox4.TabIndex = 33;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(674, 201);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 22);
+			this.textBox5.TabIndex = 34;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(879, 201);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 22);
+			this.textBox6.TabIndex = 35;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(545, 266);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 22);
+			this.textBox7.TabIndex = 36;
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(545, 391);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(100, 22);
+			this.textBox8.TabIndex = 37;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(545, 467);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 38;
+			this.label4.Text = " ";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1220, 510);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label19);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label18);
+			this.Controls.Add(this.label17);
+			this.Controls.Add(this.label16);
+			this.Controls.Add(this.label15);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Tapetess";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label19;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label18;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Tapetess/Tapetess/MainForm.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yurem
+ * Date: 24/11/2024
+ * Time: 03:46 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tapetess
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			int xd, xd2, xd3, xd4, xd5, xd6, total;
+			xd = int.Parse(textBox3.Text);
+			xd2 = int.Parse(textBox4.Text);
+			xd3 = int.Parse(textBox5.Text);
+			xd4 = int.Parse(textBox6.Text);
+			xd5 = int.Parse(textBox7.Text);
+			xd6 = int.Parse(textBox8.Text);
+			xd = xd * 370;
+			xd2 = xd2 * 250;
+			xd3 = xd3 * 200;
+			xd4 = xd4 * 450;
+			xd5 = xd5 * 9;
+			xd6 = xd6 * 455;
+			total = xd + xd2 + xd3 + xd4 + xd5 + xd6;
+			label4.Text = total.ToString();
+		}
+	}
+}
